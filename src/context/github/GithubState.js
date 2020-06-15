5fc8b24a4a3292b9e7aa822e37b845cb14d4e60f
Tushar Dhuri <tushar.dhuri@aon.com>
2020-06-15 16:36:12 +0100

    Setup variables

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -5,6 +5,17 @@ import GithubReducer from './githubReducer';
 
 import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USERS, GET_REPOS } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = (props) => {
   const initialState = {
     users: [],
@@ -18,7 +29,7 @@ const GithubState = (props) => {
   //Search Users
   const searchUsers = async (text) => {
     setLoading();
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
     dispatch({ type: SEARCH_USERS, payload: res.data.items });
   };
 
